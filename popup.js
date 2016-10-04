263974d1ad7a63b037302c150d17436717df411e
davidthurman <davidthurmanwork@gmail.com>
2016-10-04 14:18:42 -0400

    UI Changes

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   
 });
 function getImageUrl() {
-  var searchUrl = 'http://api.dvidshub.net/live/list?max_results=3&api_key=key-57f3a82240293';
+  var searchUrl = 'http://api.dvidshub.net/live/list?max_results=5&api_key=key-57f3a82240293';
   $.get( searchUrl, function( data ) {
     var title1 = data.results[0].title;
     //alert(title1);
@@ -14,10 +14,13 @@ function getImageUrl() {
         '<a href="' + value['url'] + '">' +
           '<div class="single-video-container">' +
             '<div class="container-main-section">' +
-              '<img class="thumbnail" src="' + value['thumbnail']['url'] + '"/>' +
+              '<div class="thumbnail-wrapper">' +
+                '<img class="thumbnail" src="' + value['thumbnail']['url'] + '"/>' +
+              '</div>' +
               '<div class="single-video-title">' + value['title'] + '</div>' +
               '<div class="single-video-time">' + value['begin'] + '</div>' +
             '</div>' +
+            '<div class="single-video-summary">' + value['description'] + '</div>' +
           '</div>' +
         '</a>';
 
